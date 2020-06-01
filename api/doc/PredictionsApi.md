c9ca5101a2c91e4b0ea0709e08a03da78dd7998f
Eddie Lozada <elozadaj@gmail.com>
2020-05-31 23:34:59 -0500

    Endpoints return data from DB

--- api/doc/PredictionsApi.md
+++ api/doc/PredictionsApi.md
@@ -1,8 +1,18 @@
-# PredictionsApi
+# Predictions API
 
 Method | HTTP request | Description
 ------------- | ------------- | -------------
-[**get_predictions_by_date**](PredictionsApi.md#get_predictions_by_date) | **GET** /prediction/{date} | Obtener una lista de centros para un año y mes en específico.
+[**get_predictions**](PredictionsApi.md#get_predictions) | **GET** /prediction | Obtener la lista de centros a verificar más reciente.
+[**get_predictions_by_date**](PredictionsApi.md#get_predictions_by_date) | **GET** /prediction/{date} | Obtener la lista de centros a verificar para una fecha en específico.
+
+# **get_predictions**
+> Prediction get_predictions()
+
+Obtiene la lista de centros a verificar más reciente.
+
+### Response
+
+[**Prediction**](PredictionsApi.md#Prediction)
 
 # **get_predictions_by_date**
 > Prediction get_predictions_by_date(date)
@@ -11,22 +21,21 @@ Obtiene una lista de centros para un año y mes en específico.
 
 ### Parameters
 
-Name | Type | Description  | Notes
-------------- | ------------- | ------------- | -------------
- **date** | **string**| Fecha en formato yyyy-MM | El mes debe ser válido (1-12)
+Name | Type | Description  
+------------- | ------------- | -------------
+ **date** | **string**| Fecha en formato yyyy-MM-dd
 
 ### Response
 
-[**Prediction**](Prediction.md)
-
+[**Prediction**](PredictionsApi.md#Prediction)
 
 # Prediction
 
 ## Properties
 Name | Type | Description | Notes
 ------------ | ------------- | ------------- | -------------
-**date** | **string** |  Fecha de la predicción| Formato: 'yyyy-MM' 
-**centers** | [**list[Inspection]**](Inspection.md) |  Lista de centros a inspeccionar| 
+**date** | **string** |  Fecha de la predicción| Formato: 'yyyy-MM-dd' 
+**centers** | [**list[Inspection]**](PredictionsApi.md#Inspection) |  Lista de centros a inspeccionar| 
 
 
 # Inspection
--- api/server.py
+++ api/server.py
@@ -1,33 +1,51 @@
 import re
+import configparser
+
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
 
-date_regex = re.compile(r"^[0-9]{4}-([0]?[1-9]|1[0-2])$")
+config = configparser.ConfigParser()
+config.read('config.txt')
+db_user = config['db']['user']
+db_pass = config['db']['pass']
+db_url = config['db']['url']
+db_name = config['db']['db_name']
+DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=db_user, pw=db_pass, url=db_url, db=db_name)
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # silence the deprecation warning
+db = SQLAlchemy(app)
+
+date_regex = re.compile(r"^[0-9]{4}-([0]?[1-9]|1[0-2])-[0-9]{1,2}$")
 
 
 def valid_date(date):
     return bool(date_regex.match(date))
 
 
-def get_predictions(date):
-    print(date[5])
-    if date[5] == '0':
-        return []
+def get_last_prediction_date():
+    result = db.engine.execute('SELECT DATE(MAX(date)) FROM predictions.predictions').first()
+    return str(result[0])
 
-    return [{
-        'center_id': '123xyz',
-        'ranking': '1',
-    }]
+
+def get_predictions(date):
+    db_result = db.engine.execute('SELECT * FROM predictions.predictions as PRED'
+                                  ' WHERE DATE(PRED.date) = \'{}\''
+                                  ' ORDER BY PRED.priority'.format(date)).fetchall()
+    result = []
+    for prediction in db_result:
+        result.append({'centerId': prediction[0], 'ranking': prediction[3]})
+
+    return result
 
 
 class Prediction(Resource):
 
-    def get(self, date):
-        if not valid_date(date):
-            return 'Invalid date supplied. Please follow the pattern: yyyy-MM', 400
+    def get(self):
+        date = get_last_prediction_date()
         centers = get_predictions(date)
 
         if len(centers) == 0:
@@ -36,7 +54,34 @@ class Prediction(Resource):
         return {"date": date, 'centers': centers}
 
 
-api.add_resource(Prediction, '/prediction/<date>')
+api.add_resource(Prediction, '/prediction/')
+
+
+class PredictionWithParameters(Resource):
+
+    def get(self, date):
+        if not valid_date(date):
+            return 'Invalid date supplied. Please follow the pattern: yyyy-MM-dd', 400
+
+        centers = get_predictions(date)
+        if len(centers) == 0:
+            return 'No data available for {}'.format(date), 404
+
+        return {"date": date, 'centers': centers}
+
+
+api.add_resource(PredictionWithParameters, '/prediction/<date>')
+
+
+class PredictionsModel(db.Model):
+    __tablename__ = 'predictions.predictions'
+
+    center_id = db.Column(db.String(200), primary_key=True)
+    probability = db.Column(db.Float)
+    date = db.Column(db.DateTime)
+    priority = db.Column(db.Integer)
+    matrix_uuid = db.Column(db.String(200))
+
 
 if __name__ == '__main__':
     app.run(debug=True)

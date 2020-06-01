e21d7f98fc2a3b468e1527c10a02e9fae058bcca
Gerardo Mathus <gerardo@nextia.mx>
2020-06-01 16:00:21 -0500

    add somoe charts

--- nyccci_dashboard/wsgi.py
+++ nyccci_dashboard/wsgi.py
@@ -7,40 +7,95 @@ import os
 from random import randint
 import pandas as pd
 import requests
+from datetime import datetime
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 server = flask.Flask(__name__)
 server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
 app = dash.Dash(__name__, server=server,  external_stylesheets=external_stylesheets)
 
-r = requests.get("http://18.208.188.16/prediction/")
+r = requests.get("http://localhost:3000/prediction/")
 df = pd.json_normalize(r.json(), 'centers')
-print(df)
+date_split = r.json()['date'].split('-')
+d = datetime(int(date_split[0]), int(date_split[1]), int(date_split[2]))
+date_label = "Predictions creation date: " + d.strftime(format="%b. %d, %Y")
+borough_count = df['borough'].value_counts().reset_index().rename(columns={'index':'borough','borough':'total'})
+df['fullName'] = df['centerName'].apply(lambda x: x.replace('_', ' ').title())
+type_count = df['childcareType'].value_counts().reset_index().rename(columns={'index':'childcareType','childcareType':'total'})
+colors = [
+    '#1f77b4',  # muted blue
+    '#ff7f0e',  # safety orange
+    '#2ca02c',  # cooked asparagus green
+    '#d62728',  # brick red
+    '#9467bd',  # muted purple
+    '#8c564b',  # chestnut brown
+    '#e377c2',  # raspberry yogurt pink
+    '#7f7f7f',  # middle gray
+    '#bcbd22',  # curry yellow-green
+    '#17becf'   # blue-teal
+]
 app.layout = html.Div(children=[
     html.H1(children='New York City Childcare Centers Inspections'),
-
+    html.H5(children=date_label),
     html.Div(children='''
         Centros con mayor probabilidad de presentar una violación de salud pública
     '''),
-    dash_table.DataTable(
-        id='table',
-        columns=[{"name": i, "id": i} for i in df.columns],
-        data=df.to_dict('records'),
-    ),
     dcc.Graph(
         id='example-graph',
         figure={
             'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+                {
+                    'x': borough_count['borough'].values,
+                    'y': borough_count['total'].values,
+                    'type': 'bar',
+                    'name': 'Borough',
+                    'marker': {'color': colors}
+                },
             ],
             'layout': {
-                'title': 'Dash Data Visualization'
+                'title': 'Frecuencia absoluta de los distritos en donde están los centros que requieren inspección'
             }
         }
-    )
-
-], style={'columnCount': 2})
+    ),
+    dcc.Graph(
+        id='childcare-type-graph',
+        figure={
+            'data': [
+                {
+                    'x': type_count['childcareType'].values,
+                    'y': type_count['total'].values,
+                    'type': 'bar',
+                    'name': 'childcareType',
+                    'marker': {'color': colors}
+                }
+            ],
+            'layout': {
+                'title': 'Tipo de guardería'
+            }
+        }
+    ),
+    dash_table.DataTable(
+        id='centersTable',
+        columns=[{"name": i, "id": i} for i in df[['fullName', 'probability_str', 'borough']].columns],
+        data=df[['fullName', 'probability_str', 'borough']].to_dict('records'),
+        style_table={
+            'width': '60%',
+        },
+        style_cell={
+                'textAlign': 'left'
+        },
+        style_data_conditional=[
+            {
+                'if': {'row_index': 'odd'},
+                'backgroundColor': 'rgb(248, 248, 248)'
+            }
+        ],
+        style_header={
+        'backgroundColor': 'rgb(230, 230, 230)',
+        'fontWeight': 'bold'
+        }
+    ),
+])
 
 
 # Run the Dash app

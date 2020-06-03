11a89118ed612ca4208c70f66a791307f230110f
Gerardo Mathus <gerardo@nextia.mx>
2020-06-02 22:28:10 -0500

    delete devs

--- orquestador/bin/clear_bucket_dev.sh
+++
@@ -1,1 +0,0 @@
-aws s3 rm s3://nyc-ccci --recursive --profile=mathus_itam
--- orquestador/bin/run_orchestrator_dev.sh
+++
@@ -1,1 +0,0 @@
-PYTHONPATH='.' AWS_PROFILE=mathus_itam luigi --log-level=INFO --module nyc_ccci_etl.orchestrator_tasks.pipeline_root PipelineRoot --pipeline-type=$1 --year=$2 --month=$3 --day=$4
